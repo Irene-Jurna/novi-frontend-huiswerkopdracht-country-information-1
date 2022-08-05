b22950898243be7980bcf3a8ebd188fa00ca5240
Irene Jurna <irene.jurna@gmail.com>
2022-08-05 11:52:42 +0200

    start-huiswerkopdracht

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+import axios from "axios";
+
 console.log('Hallo daar!');
+
+const countryList = document.getElementById('countries');
+
+async function fetchCountries() {
+    try {
+        // het request maken
+        const response = await axios.get('https://restcountries.com/v2/all');
+        // console.log(response.data[0].languages[1].name);
+
+        // countryList.innerHTML = `
+        // <li>Een van de talen is: ${response.data[0].languages[1].name} </li>
+        // `;
+    } catch(e) {
+        // de errors afvangen
+        console.error(e);
+    }
+}
+
+fetchCountries();
