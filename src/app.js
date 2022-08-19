d8115d5bb1182cc21356189c38607a3fd04dc85e
Irene Jurna <irene.jurna@gmail.com>
2022-08-19 09:54:56 +0200

    huiswerkopdracht v1

--- src/app.js
+++ src/app.js
@@ -1,22 +1,57 @@
 import axios from "axios";
 
-console.log('Hallo daar!');
-
 const countryList = document.getElementById('countries');
+
+function colorCountries(region) {
+    let continentColor = '';
+
+    switch (region) {
+        case 'Asia':
+            continentColor = 'continent-list-asia';
+            break;
+        case 'Americas':
+            continentColor = 'continent-list-americas';
+            break;
+        case 'Africa':
+            continentColor = 'continent-list-africa';
+            break;
+        case 'Europe':
+            continentColor = 'continent-list-europe';
+            break;
+        case 'Oceania':
+            continentColor = 'continent-list-oceania';
+            break;
+        default:
+            continentColor = 'continent-list-default';
+    }
+    return continentColor;
+};
 
 async function fetchCountries() {
     try {
         // het request maken
         const response = await axios.get('https://restcountries.com/v2/all');
-        // console.log(response.data[0].languages[1].name);
+        console.log(response.data);
 
-        // countryList.innerHTML = `
-        // <li>Een van de talen is: ${response.data[0].languages[1].name} </li>
-        // `;
-    } catch(e) {
+        response.data.sort((a, b) => a.population - b.population);
+
+        const listItems = response.data.map(({name, population, region, flag}) => {
+            let color = colorCountries(region);
+            return `<li class="${color}"><img src="${flag}" alt="Flag" width="25"> ${name}</li>
+            <li class="countries-list">Has a population of ${population} people</li>`
+        });
+        countryList.innerHTML = listItems.join('');
+
+    } catch (e) {
         // de errors afvangen
         console.error(e);
     }
-}
+};
 
 fetchCountries();
+
+
+//testen voor 1 entry
+// countryList.innerHTML = `
+// <li> <span> ${response.data[0]} </span> <span> Has a population of ${response.data[0]} people</span></li>
+// `;
