b865576e550b692eda8bb94bb13f12863faaaa91
Irene Jurna <irene.jurna@gmail.com>
2022-09-06 20:53:10 +0200

    huiswerkopdracht

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -576,10 +576,55 @@ async function fetchCountries() {
         console.error(e);
     }
 }
-fetchCountries(); //testen voor 1 entry
- // countryList.innerHTML = `
- // <li> <span> ${response.data[0]} </span> <span> Has a population of ${response.data[0]} people</span></li>
- // `;
+fetchCountries();
+//testen voor 1 entry
+// countryList.innerHTML = `
+// <li> <span> ${response.data[0]} </span> <span> Has a population of ${response.data[0]} people</span></li>
+// `;
+// PART 2
+const countryData = document.getElementById("country-searcher-output");
+const searchForm = document.getElementById("search-form");
+searchForm.addEventListener("submit", searchName);
+function searchName(e) {
+    e.preventDefault();
+    const searchInput = document.getElementById("search-input");
+    searchCountries(searchInput.value);
+    searchInput.value = "";
+}
+async function searchCountries(name) {
+    try {
+        const output = await (0, _axiosDefault.default).get(`https://restcountries.com/v2/name/${name}`);
+        const country = output.data[0];
+        console.log(country);
+        countryData.innerHTML = `<span> 
+            <h2><img src="${country.flag}" width=50 />${country.name}</h2> 
+            <p>${country.name} is situated in ${country.subregion}. 
+            It has a population of ${country.population} people.</p>
+            <p>The capital is ${country.capital} ${countryCurrencies(country.currencies)}.</p>
+            <p>They speak ${countryLanguages(country.languages)}</p>
+            </span>`;
+    } catch (e) {
+        countryData.innerHTML = `<p>Error. ${name} is een onbekend land. Vul een geldige zoekopdracht in. </p>`;
+    }
+}
+function countryCurrencies(currencies) {
+    let result = "and you can pay with ";
+    if (currencies.length === 2) return result + `${currencies[0].name} and ${currencies[1].name}`;
+    return result + `${currencies[0].name}`;
+}
+function countryLanguages(languages) {
+    let ret = "";
+    for(let i = 0; i < languages.length; i++){
+        if (languages.length === 1) {
+            ret = languages[i].name;
+            break;
+        }
+        if (i === languages.length - 1) ret += " and ";
+        else if (i > 0) ret += ", ";
+        ret += languages[i].name;
+    }
+    return ret;
+}
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 module.exports = require("./lib/axios");
